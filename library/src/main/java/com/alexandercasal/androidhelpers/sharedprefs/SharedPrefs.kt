76825b373d6f251ac3d6381ba18b7a9c60f3c360
Alexander Casal <alexandericasal@gmail.com>
2019-01-26 20:06:29 -0700

    1.0-beta08 Add LocalTimePref

--- library/src/main/java/com/alexandercasal/androidhelpers/sharedprefs/SharedPrefs.kt
+++ library/src/main/java/com/alexandercasal/androidhelpers/sharedprefs/SharedPrefs.kt
@@ -5,9 +5,13 @@ import androidx.lifecycle.LiveData
 import com.alexandercasal.androidhelpers.sharedprefs.livedata.BooleanSharedPrefsLiveData
 import com.alexandercasal.androidhelpers.sharedprefs.livedata.FloatSharedPrefsLiveData
 import com.alexandercasal.androidhelpers.sharedprefs.livedata.IntSharedPrefsLiveData
+import com.alexandercasal.androidhelpers.sharedprefs.livedata.LocalTimePrefsLiveData
 import com.alexandercasal.androidhelpers.sharedprefs.livedata.LongSharedPrefsLiveData
 import com.alexandercasal.androidhelpers.sharedprefs.livedata.StringSetSharedPrefsLiveData
 import com.alexandercasal.androidhelpers.sharedprefs.livedata.StringSharedPrefsLiveData
+import org.threeten.bp.LocalTime
+import org.threeten.bp.format.DateTimeFormatter
+import timber.log.Timber
 import java.lang.IllegalArgumentException
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
@@ -86,5 +90,47 @@ abstract class SharedPrefs(private val context: Context, private val fileName: S
         }
 
         fun toLiveData(): LiveData<Set<String>?> = StringSetSharedPrefsLiveData(sharedPreferences, defValue, key)
+    }
+
+    /**
+     * Manages storing and retrieving a [LocalTime][org.threeten.bp.LocalTime] directly as a shared preference.
+     * The LocalTime will be stored as a [String] formatted in [ISO_LOCAL_TIME][org.threeten.bp.format.DateTimeFormatter.ISO_LOCAL_TIME].
+     *
+     * While malformatted time strings will be guarded against, any indirect operations on this preference that result in a differently
+     * formatted String may result in unexpected behavior.
+     */
+    @Suppress("MemberVisibilityCanBePrivate")
+    inner class LocalTimePref(val defValue: LocalTime?, val key: String) : ReadWriteProperty<SharedPrefs, LocalTime?> {
+        var value: LocalTime? = defValue
+
+        override fun getValue(thisRef: SharedPrefs, property: KProperty<*>): LocalTime? {
+            val timeString: String? = sharedPreferences.getString(key, defValue?.format(DateTimeUtil.localTimeFormatter))
+            timeString ?: return null
+
+            return DateTimeUtil.toLocalTime(timeString, value)
+        }
+
+        override fun setValue(thisRef: SharedPrefs, property: KProperty<*>, value: LocalTime?) {
+            if (this.value != value) {
+                this.value = value
+                sharedPreferences.edit().putString(key, value?.format(DateTimeUtil.localTimeFormatter)).apply()
+            }
+        }
+
+        fun toLiveData(): LiveData<LocalTime?> = LocalTimePrefsLiveData(sharedPreferences, defValue, key)
+    }
+
+    internal object DateTimeUtil {
+
+        val localTimeFormatter: DateTimeFormatter = DateTimeFormatter.ISO_LOCAL_TIME
+
+        fun toLocalTime(time: String?, fallback: LocalTime?): LocalTime? {
+            return try {
+                LocalTime.parse(time, localTimeFormatter)
+            } catch (e: Exception) {
+                Timber.e(e, "Malformatted LocalTime string: $time")
+                fallback // fall back to the last known good LocalTime
+            }
+        }
     }
 }
